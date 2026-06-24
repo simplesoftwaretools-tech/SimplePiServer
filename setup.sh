d4b915723a767deed70515db61b2ae3c0e139b5b
simplesoftwaretools-tech <simplesoftwaretools@gmail.com>
2026-06-24 14:43:44 -0400

    Fix setup file.

--- setup.sh
+++ setup.sh
@@ -7,10 +7,11 @@ sudo systemctl start nginx
 sudo systemctl enable nginx
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
-cd "$DIR/serverApp"
 
-python3 -m venv venv
-source venv/bin/activate
+python3 -m venv "$SCRIPT_DIR/venv"
+source "$SCRIPT_DIR/venv/bin/activate"
 pip install fastapi uvicorn python-multipart aiofiles
+
+cd "$DIR/serverApp"
 
 uvicorn main:app --host 0.0.0.0 --port 8000
