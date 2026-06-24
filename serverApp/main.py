29d1de224d95f3a227fc907be2d982c29769b2b2
simplesoftwaretools-tech <simplesoftwaretools@gmail.com>
2026-06-24 13:57:35 -0400

    Setup python script.

---
+++ serverApp/main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get("/", response_class=HTMLResponse)
+async def home():
+    return "<h1>Hello World</h1>"
