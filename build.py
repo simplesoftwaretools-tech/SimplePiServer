351b9c22423b3849f386b370fa31bbfa935333e5
simplesoftwaretools-tech <simplesoftwaretools@gmail.com>
2026-06-24 14:16:50 -0400

    Delete build.py

--- build.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-subprocess.run(["sudo","apt","update"])
-subprocess.run(["sudo","apt","install","nginx","-y"])
-subprocess.run(["sudo","systemctl","start","nginx"])
-subprocess.run(["sudo","systemctl","enable","nginx"])
